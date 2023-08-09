4587c71a8e337622d9aff7e20060564d7f250718
Mijin <mjkim1293@gmail.com>
2023-08-09 20:07:50 +0900

    [김미진] 테스트 view

--- cakecraft/src/main/java/com/goodee/cakecraft/controller/TestController.java
+++ cakecraft/src/main/java/com/goodee/cakecraft/controller/TestController.java
@@ -2,6 +2,7 @@ package com.goodee.cakecraft.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -11,10 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 public class TestController {
 	@Autowired CommonController commonController;
 	
-	@GetMapping("/code")
-	public String getCode(String name) {
+	@GetMapping("/test")
+	public String getCode(Model model, String name) {
 		String code = commonController.getCode("인사팀");
 		log.debug(code);
-		return code;
+		model.addAttribute(code);
+		return "/test";
 	}
 }
