c62768bc1ac7dd2a2a63dd76d057a1ad13497768
electricPo <electricPotatoGarden@gmail.com>
2023-08-09 11:41:09 +0900

    [김미선] Emp.java / vo클래스 생성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/Emp.java
@@ -0,0 +1,20 @@
+package com.goodee.cakecraft.vo;
+
+public class Emp {
+	private String id;
+	private String name;
+	private String socialNo;
+	private String deptCd;
+	private String teamCd;
+	private String position;
+	private String email;
+	private String hireDate;
+	private String retireDate;
+	private String empStatus;
+	private int dayoffCnt;
+	private String address;
+	private String regDtime;
+	private String modDtime;
+	private String regId;
+	private String modId;
+}
