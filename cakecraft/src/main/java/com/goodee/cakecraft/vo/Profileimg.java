f0fc993d5e03d2d4c7a6a76f897a69fb10abe092
electricPo <electricPotatoGarden@gmail.com>
2023-08-09 12:00:46 +0900

    [김미선] Profileimg.java / vo 클래스 생성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/Profileimg.java
@@ -0,0 +1,15 @@
+package com.goodee.cakecraft.vo;
+
+import lombok.Data;
+
+@Data
+public class Profileimg {
+	private String id;
+	private String profileFilename;
+	private String profilePath;
+	private String profileType;
+	private String regDtime;
+	private String modDtime;
+	private String regId;
+	private String modId;
+}
