f4c1e81b0eb1313cfe455c5c4e8d52dacc704439
ggyeah <ggyeah34@naver.com>
2023-08-09 11:45:46 +0900

    [임지예] Dayoff.java / vo 클래스생성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/Dayoff.java
@@ -0,0 +1,13 @@
+package com.goodee.cakecraft.vo;
+
+import lombok.Data;
+
+@Data
+public class Dayoff {
+	private String id;
+	private String pw;
+	private String regDtime;
+	private String modDtime;
+	private String regId;
+	private String modId;
+}
