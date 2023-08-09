792a9e0079526055260c30ad32cfac04b327de94
ggyeah <ggyeah34@naver.com>
2023-08-09 11:49:04 +0900

    [임지예] vo 클래스 생성

--- cakecraft/src/main/java/com/goodee/cakecraft/vo/IdList.java
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/IdList.java
@@ -3,12 +3,9 @@ package com.goodee.cakecraft.vo;
 import lombok.Data;
 
 @Data
-public class Dayoff {
-	private String dayoffNo;
+public class IdList {
 	private String id;
-	private String dayoffStatus;
-	private String startDay;
-	private String endDay;
+	private String pw;
 	private String regDtime;
 	private String modDtime;
 	private String regId;
---
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/StStdCd.java
@@ -0,0 +1,15 @@
+package com.goodee.cakecraft.vo;
+
+import lombok.Data;
+
+@Data
+public class StStdCd {
+	private String grpCd;
+	private int cd;
+	private String cdNm;
+	private String useYn;
+	private String regDtime;
+	private String modDtime;
+	private String regId;
+	private String modId;
+}
