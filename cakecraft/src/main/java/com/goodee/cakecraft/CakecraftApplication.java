8932b9ae33e8b11fdcd61933eea030c18233482b
Mijin <mjkim1293@gmail.com>
2023-08-09 11:01:25 +0900

    [김미진] 프로젝트 생성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/CakecraftApplication.java
@@ -0,0 +1,13 @@
+package com.goodee.cakecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakecraftApplication.class, args);
+	}
+
+}
---
+++ cakecraft/src/test/java/com/goodee/cakecraft/CakecraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodee.cakecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakecraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
