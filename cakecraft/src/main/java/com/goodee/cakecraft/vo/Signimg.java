839672ddc3983fa633f6ef4c702c57ed2c94f705
electricPo <electricPotatoGarden@gmail.com>
2023-08-09 11:54:54 +0900

    [김미선] Signimg.java / vo 클래스 생성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/Signimg.java
@@ -0,0 +1,15 @@
+package com.goodee.cakecraft.vo;
+
+import lombok.Data;
+
+@Data
+public class Signimg {
+	private String id;
+	private String signFilename;
+	private String signPath;
+	private String signType;
+	private String regDtime;
+	private String modDtime;
+	private String regId;
+	private String modId;
+}
