d93950d1b37c2a37864cbf51526efc0a02501bf2
Mijin <mjkim1293@gmail.com>
2023-08-09 11:30:51 +0900

    [김미진] Reservation.java / vo클래스 생성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/Reservation.java
@@ -0,0 +1,18 @@
+package com.goodee.cakecraft.vo;
+
+import lombok.Data;
+
+@Data
+public class Reservation {
+	private int reservationNo;
+	private int facilityNo;
+	private String teamCd;
+	private String id;
+	private String reservationContent;
+	private String startDtime;
+	private String endDtime;
+	private String regDtime;
+	private String modDtime;
+	private String regId;
+	private String modId;
+}
