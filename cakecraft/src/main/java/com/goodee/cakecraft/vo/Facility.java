de283ab0c0dc0a1eca3775c03391ab3841e19ddd
Mijin <mjkim1293@gmail.com>
2023-08-09 11:25:12 +0900

    [김미진] Facility.java / vo클래스 생성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/Facility.java
@@ -0,0 +1,15 @@
+package com.goodee.cakecraft.vo;
+
+import lombok.Data;
+
+@Data
+public class Facility {
+	private int facilityNo;
+	private String categoryCd;
+	private String facilityNote;
+	private String useYn;
+	private String regDtime;
+	private String modDtime;
+	private String regId;
+	private String modId;
+}
