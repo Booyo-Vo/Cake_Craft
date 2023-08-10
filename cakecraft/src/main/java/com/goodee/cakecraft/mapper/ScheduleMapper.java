87fdfc7bffe336ace357cc6d33d557a94ad70783
ueuye <eu99ne@gmail.com>
2023-08-10 15:51:41 +0900

    [구은혜] ScheduleMapper / CRUD쿼리작성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/mapper/ScheduleMapper.java
@@ -0,0 +1,23 @@
+package com.goodee.cakecraft.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.goodee.cakecraft.vo.ScheduleBase;
+
+public interface ScheduleMapper {
+	// 월간 일정 목록 출력 (달력에 일정 출력)
+	List<ScheduleBase> selectScheduleListByMonth(Map<String, Object> map);
+	
+	// 개별 일정 출력
+	ScheduleBase selectScheduleByNo(ScheduleBase schedule);
+	
+	// 일정 추가 
+	int insertSchedule(ScheduleBase schedule);
+	
+	// 일정 수정
+	int updateSchedule(ScheduleBase schedule);
+	
+	// 일정 삭제
+	int deleteSchedule(ScheduleBase schedule);
+}
