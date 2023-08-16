9c681d08098573b72ed078f33a24ac279d8d631a
ggyeah <ggyeah34@naver.com>
2023-08-16 11:58:11 +0900

    차트에 표시할 데이터 출력

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/controller/ChartController.java
@@ -0,0 +1,80 @@
+package com.goodee.cakecraft.controller;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.goodee.cakecraft.service.ChartService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class ChartController {
+	//ANSI코드
+	final String GREEN = "\u001B[42m";
+	final String RESET = "\u001B[0m";
+	
+	@Autowired
+	private ChartService chartService;
+	
+	// 차트리스트
+	@GetMapping("/emp/chartList")
+	public String yearCnt(Model model,  
+						@RequestParam(required = false) String month,
+						@RequestParam(required = false) String year) {
+	// 선택된 년도와 월이 없다면 현재 년도와 월 가져오기
+    if (year == null || year.isEmpty()) {
+        Calendar calendar = Calendar.getInstance();
+        year = String.valueOf(calendar.get(Calendar.YEAR));
+        log.debug(GREEN +"chartList year:"+year +RESET);
+    }
+
+    if (month == null || month.isEmpty()) {
+        Calendar calendar = Calendar.getInstance();
+        month = String.valueOf(calendar.get(Calendar.MONTH) + 1); // 월은 0부터 시작하므로 1을 더해줌
+        log.debug(GREEN +"chartList month:"+month +RESET);
+    }
+		
+	//해당년도 입사자 퇴사자
+	Map<String, Object> YearCntMap= chartService.getYearCnt(year);
+
+	//해당월 입사자 퇴사자
+	Map<String, Object> MonthCntMap= chartService.getMonthCnt(month);
+	
+	//직급별 인원수
+    List<Map<String, Object>> positionCnt = chartService.getPositionCnt();
+    
+    //부서별 인원수
+    List<Map<String, Object>> deptCnt = chartService.getDeptCnt();
+    
+    //팀별 인원수
+    List<Map<String, Object>> teamCnt = chartService.getTeamCnt();
+    
+    //성별 인원수
+    List<Map<String, Object>> genderCnt = chartService.getGenderCnt();
+    
+    
+    //view로 넘길때는 다시 분리해서
+	model.addAttribute("hireYearCnt", YearCntMap.get("hireYearCnt"));
+	model.addAttribute("retireYearCnt", YearCntMap.get("retireYearCnt"));
+	model.addAttribute("hireMonthCnt", MonthCntMap.get("hireMonthCnt"));
+	model.addAttribute("retireMonthCnt", MonthCntMap.get("retireMonthCnt"));
+	model.addAttribute("positionCnt", positionCnt);
+	model.addAttribute("deptCnt", deptCnt);
+	model.addAttribute("teamCnt", teamCnt);
+	model.addAttribute("year", year);
+	model.addAttribute("month", month);
+	model.addAttribute("genderCnt", genderCnt);
+	
+	return "/emp/chartList";
+	}
+}
+	
---
+++ cakecraft/src/main/webapp/WEB-INF/view/emp/chartList.jsp
@@ -0,0 +1,64 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>chartList</title>
+</head>
+<body>
+    ${month} 월 입사자 퇴사자
+    <br>
+    재직자 수: ${hireMonthCnt}
+    퇴사자 수: ${retireMonthCnt}
+    <br>
+     ${year}년 입사자 퇴사자
+    <br>
+    재직자 수: ${hireYearCnt}
+    퇴사자 수: ${retireYearCnt}
+    <br>
+    직급별 인원 수 
+    <table border="1">
+        <tr>
+            <th>직급</th>
+            <th>인원</th>
+        </tr>
+        <c:forEach items="${positionCnt}" var="position">
+            <tr>
+                <td>${position.positionName}</td>
+                <td>${position.positionCnt}</td>
+            </tr>
+        </c:forEach>
+    </table>
+    부서별 인원 수 
+    <table border="1">
+        <tr>
+            <th>부서</th>
+            <th>인원</th>
+        </tr>
+        <c:forEach items="${deptCnt}" var="dept">
+            <tr>
+                <td>${dept.deptName}</td>
+                <td>${dept.deptCnt}</td>
+            </tr>
+        </c:forEach>
+    </table>
+     팀별 인원 수 
+    <table border="1">
+        <tr>
+            <th>팀</th>
+            <th>인원</th>
+        </tr>
+        <c:forEach items="${teamCnt}" var="team">
+            <tr>
+                <td>${team.teamName}</td>
+                <td>${team.teamCnt}</td>
+            </tr>
+        </c:forEach>
+    </table>
+     <c:forEach items="${genderCnt}" var="genderCnt">
+    남자${genderCnt.maleCount}
+    여자${genderCnt.femaleCount}
+	</c:forEach>
+</body>
+</html>
