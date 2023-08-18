513f0b4d7cbeb770be39b73ea9529f7665b3639b
Mijin <mjkim1293@gmail.com>
2023-08-18 17:35:51 +0900

    [김미진] 현재시간 기준 예약시간 보기 및 예약삭제 기능추가

--- cakecraft/src/main/java/com/goodee/cakecraft/mapper/ReservationMapper.java
+++ cakecraft/src/main/java/com/goodee/cakecraft/mapper/ReservationMapper.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.goodee.cakecraft.vo.EmpIdList;
 import com.goodee.cakecraft.vo.FacilityReservation;
 
 @Mapper
@@ -13,7 +14,7 @@ public interface ReservationMapper {
 	List<FacilityReservation> selectReservationListByDate(Map<String, Object> paramMap);
 	
 	//id별 예약이력 가져오기
-	List<FacilityReservation> selectReservationListById(Map<String, Object> paramMap);
+	List<Map<String, Object>> selectReservationListById(EmpIdList emp);
 	
 	//시설비품별 예약가능 시간
 	List<Map<String, Object>> selectReservationTime(Map<String, Object> paramMap);
@@ -21,10 +22,9 @@ public interface ReservationMapper {
 	//예약하기
 	int insertReservation(Map<String, Object> paramMap);
 	
-	//예약 수정하기
-	int updateReservation(Map<String, Object> paramMap);
-	
 	//예약 취소하기
-	int deleteReservation(Map<String, Object> paramMap);
+	int deleteReservation(FacilityReservation reservation);
 	
+	//1시간 뒤 구하기
+	String selectAnHourLater();
 }
