a5b9796ab96864a21bdb00a9468aa47a7858e7b1
electricPo <electricPotatoGarden@gmail.com>
2023-08-21 10:45:03 +0900

    [김미선] EmpAttendance / vo 클래스 생성

---
+++ cakecraft/src/main/java/com/goodee/cakecraft/vo/EmpAttendance.java
@@ -0,0 +1,15 @@
+package com.goodee.cakecraft.vo;
+
+import lombok.Data;
+
+@Data
+public class EmpAttendance {
+	private int attendanceNo;
+	private String id;
+	private String startDtime;
+	private String endDtime;
+	private String regDtime;
+	private String modDtime;
+	private String regId;
+	private String modId;
+}
